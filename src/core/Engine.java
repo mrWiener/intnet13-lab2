1faaab45623b4883e152c09d1833f29968855cd5
Lucas Wiener <lucas@wiener.se>
2013-02-11 23:08:23 +0100

    Now handling cookies better

--- src/core/Engine.java
+++ src/core/Engine.java
@@ -1,14 +1,21 @@
 package core;
 
-import java.io.*;
-import java.net.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Engine {
 	private GameManager games;
+	Pattern cookieIdPattern;
 
 	public Engine() throws IOException {
 		games = new GameManager();
+		cookieIdPattern = Pattern.compile("gameId=\\d+");
 		
 		run();
 	}
@@ -26,12 +33,21 @@ public class Engine {
 			
 			String str = request.readLine();
 			System.out.println(str);
-			StringTokenizer tokens = new StringTokenizer(str, " ?");
+			/*StringTokenizer tokens = new StringTokenizer(str, " ?");
 			tokens.nextToken(); // Ordet GET
-			String requestedDocument = tokens.nextToken();
+			String requestedDocument = tokens.nextToken();*/
 			
 			while ((str = request.readLine()) != null && str.length() > 0) {
 				System.out.println(str);
+			
+				if(str.matches("^Cookie:.*gameId=\\d+")) {
+					Matcher m = cookieIdPattern.matcher(str);
+					
+					if (m.find()) {
+					    int id = Integer.parseInt(m.group(0).split("=")[1]);
+						System.out.println("id: " + id);
+					}
+				}
 			}
 			
 			System.out.println("Förfrågan klar.");
@@ -41,10 +57,10 @@ public class Engine {
 			response.println("HTTP/1.1 200 OK");
 			response.println("Server : Guess game");
 			
-			if (requestedDocument.indexOf(".html") != -1)
+			/*if (requestedDocument.indexOf(".html") != -1)
 				response.println("Content-Type: text/html");
-
-			response.println("Set-Cookie: clientId=1; expires=Wednesday,31-Dec-13 21:00:00 GMT");
+*/
+			response.println("Set-Cookie: gameId=113131; expires=Wednesday,31-Dec-13 21:00:00 GMT");
 
 			response.println();
 			
